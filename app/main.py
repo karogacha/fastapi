31a959d9a2eedc40571d3dc8787de220c5f1398d
karogacha <karen.rocio@gmail.com>
2023-10-17 18:13:38 -0600

    Create tables from models

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from .config import settings
 
 
 # if you use alembic, this is not necessary
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
